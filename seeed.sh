686ade212ed1e693a9b913d129e460a752d2eeba
Ubuntu <ubuntu@cad.lxd>
2017-11-27 23:31:53 +0000

    Some final changes

---
+++ seeed.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# change file names to those seeedstudio wants
+FROM_PATH=gerber
+TO_PATH=seed-gerber
+FROM_PCBNAME=wififem
+PCBNAME=wififem
+
+FROM=$FROM_PATH/$FROM_PCBNAME
+TO=$TO_PATH/$PCBNAME
+mkdir $TO_PATH
+mv $FROM-B.Cu.gbl $TO.GBL
+mv $FROM-B.Mask.gbs $TO.GBS
+mv $FROM-B.SilkS.gbo $TO.GBO
+mv $FROM-F.Cu.gtl $TO.GTL
+mv $FROM-F.Mask.gts $TO.GTS
+mv $FROM-F.SilkS.gto $TO.GTO
+#mv $FROM-In1.Cu.g2 $TO.GL2
+#mv $FROM-In2.Cu.g3 $TO.GL3
+mv $FROM.drl $TO.TXT
+mv $FROM-Edge.Cuts.gm1 $TO.GML
+cd $TO_PATH
+#zip -r $PCBNAME.zip $PCBNAME.GTL $PCBNAME.GBL $PCBNAME.GTS $PCBNAME.GBS $PCBNAME.GTO $PCBNAME.GBO $PCBNAME.GL2 $PCBNAME.GL3 $PCBNAME.TXT $PCBNAME.GML
+zip -r $PCBNAME.zip $PCBNAME.GTL $PCBNAME.GBL $PCBNAME.GTS $PCBNAME.GBS $PCBNAME.GTO $PCBNAME.GBO $PCBNAME.TXT $PCBNAME.GML
